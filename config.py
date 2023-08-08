bbc5fdf720ed79c0fdde0dddfc5d3bfa0924f2d8
RITESH <108003920+ritesh-0@users.noreply.github.com>
2023-08-08 19:59:40 +0530

    Update config.py

--- config.py
+++ config.py
@@ -16,7 +16,7 @@ class Config(object):
     BOT_UPTIME  = time.time()
     START_PIC   = os.environ.get("START_PIC", "https://telegra.ph/file/3b7050b8b5d918a00b490.jpg")
     ADMIN       = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get('ADMIN', '').split()]
-    FORCE_SUB   = os.environ.get("FORCE_SUB", "Roofiverse") 
+    FORCE_SUB   = os.environ.get("FORCE_SUB", "") 
     LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", ""))
     FLOOD = int(os.environ.get("FLOOD", '10'))
 
